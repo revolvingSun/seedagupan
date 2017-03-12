db32dc606780b7406d1eed154cc51342ddece93a
revolvingSun <igani_garcia@yahoo.com>
2017-03-12 15:49:55 +0800

    second revision

---
+++ js/myscript.js
@@ -0,0 +1,27 @@
+var resto = [];
+
+resto[0] = {
+		["name"]: "Sizling County",
+		["address"]: "Unit 9, Mother Goose Building, Dagupan - Binmaley Rd, Barangay",
+		["phone"]: [],
+		["opening hour"]:  "7:00",
+		["closing hour"]:  "22:00",
+		["days open"]: "",
+		["specialty"]: ["grill"],
+		["latitude"]: "",
+		["longitude"]: "",
+		
+}
+
+resto[1] ={
+		["name"]: "City De Luxe",
+		["address"]: "Unit 9, Mother Goose Building, Dagupan - Binmaley Rd, Barangay",
+		["phone"]: [],
+		["opening hour"]:  "7:00",
+		["closing hour"]:  "22:00",
+		["days open"]: "",
+		["specialty"]: ["grill"],
+		["latitude"]: "",
+		["longitude"]: "",
+	
+}
